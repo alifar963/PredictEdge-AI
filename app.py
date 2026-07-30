88b6e63447363b1b2039aa1a31152dfd07ff1214
alifar963 <alifar963@gmail.com>
2026-07-30 10:26:46 +0300

    Update app version to v3.0

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ Electrical Engineer
 
 ---
 
-Version **2.0**
+Version **3.0**
 """)
     st.info("This dashboard is intended for predictive maintenance demonstrations. Predictions are to support inspection and maintenance decisions not replace them.")
 
@@ -1154,7 +1154,7 @@ Recommended Actions:
 
 st.divider()
 st.caption(
-     "PredictEdge AI v2.0   |  Developed by Ali Farooq   | "
+     "PredictEdge AI v3.0   |  Developed by Ali Farooq   | "
      "Python • Streamlit • Scikit-learn • SHAP"
 )
 print(esp_model.classes_)
